c17eee0502d72d1d728f4e4098758548af03db98
Brass Tax <brasstax@users.noreply.github.com>
2017-06-03 14:31:42 -0700

    Added folder for utilities functions and ffrkcentral_data folder.

--- ffrkbot.js
+++ ffrkbot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs');
 const path = require('path');
+const utils = require('./utilities/utilities');
 
 const configPath = path.join(__dirname, 'config.json');
 
