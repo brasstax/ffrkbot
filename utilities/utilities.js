6c7d0bd5d119da4076a5eb18be3d643df7cd7544
Brass Tax <brasstax@users.noreply.github.com>
2017-06-03 18:12:02 -0700

    You can now extend bot functionality by adding a global.<command_name> = function command_name to utilities.js.

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -3,10 +3,24 @@ exports.parseMsg = function(msg) {
   if (msg.content.startsWith(msgPrefix)) {
     let msgLower = msg.content.toLowerCase();
     let commandWithArgs = msgLower.split(' ');
-    let command = commandWithArgs[0];
+    console.log('commandWithArgs: ' + commandWithArgs);
+    let command = commandWithArgs[0].slice(1);
     let args = [];
-    if (commandWithArgs.shift().length() > 0) {
+    console.log('Command is ' + command);
+    if (commandWithArgs.shift().length > 0) {
       args = commandWithArgs;
+      console.log('Args are ' + args);
+    };
+    try {
+      global[command](msg, args);
+    } catch (e) {
+      console.log('Command does not exist: ' + command);
     };
   };
 };
+
+global.ping = function ping(msg, args) {
+  msg.reply('pong');
+  return;
+};
+
