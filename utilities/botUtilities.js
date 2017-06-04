6a37d2e5d877428e0b782218a2b8526f7d043028
Brass Tax <brasstax@users.noreply.github.com>
2017-06-03 18:29:40 -0700

    botUtilities now logs the calling user.

--- utilities/botUtilities.js
+++ utilities/botUtilities.js
@@ -1,16 +1,21 @@
+const util = require('util');
+
 exports.parseMsg = function(msg) {
   let msgPrefix = '!';
   if (msg.content.startsWith(msgPrefix)) {
     let msgLower = msg.content.toLowerCase();
     let commandWithArgs = msgLower.split(' ');
-    console.log('commandWithArgs: ' + commandWithArgs);
+    console.log(util.format('commandWithArgs: %s', commandWithArgs));
+    // Creates the command, removes the msgPrefix
     let command = commandWithArgs[0].slice(1);
     let args = [];
-    console.log('Command is ' + command);
+    console.log(util.format('command: %s', command));
     if (commandWithArgs.shift().length > 0) {
       args = commandWithArgs;
-      console.log('Args are ' + args);
+      console.log(util.format('args: %s', args));
     };
+    console.log(util.format('caller: %s#%s',
+      msg.author.username, msg.author.discriminator));
     try {
       global[command](msg, args);
     } catch (e) {
