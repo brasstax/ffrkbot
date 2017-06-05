65986ab8a071562e9745d0f900520059e283cc54
Brass Tax <brasstax@users.noreply.github.com>
2017-06-05 00:04:30 -0700

    reverted accidental commit with discord-js.commando requirement.

--- ffrkbot.js
+++ ffrkbot.js
@@ -1,4 +1,5 @@
-const Commando = require('discord.js-commando');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const fs = require('fs');
 const path = require('path');
 const botUtils = require('./utilities/botUtilities.js');
@@ -8,11 +9,6 @@ const configPath = path.join(__dirname, 'config.json');
 const configFile = fs.readFileSync(configPath);
 const config = JSON.parse(configFile);
 const token = config.token;
-const owner = config.owner;
-
-const client = new Commando.Client({
-  owner: owner,
-});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
