8e7b0eab2378f7a3c7e6076c2a5b9ebf6aead711
Brass Tax <brasstax@users.noreply.github.com>
2017-06-04 21:40:27 -0700

    Replaced client with discord.js-commando instead.

--- ffrkbot.js
+++ ffrkbot.js
@@ -1,5 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Commando = require('discord.js-commando');
 const fs = require('fs');
 const path = require('path');
 const botUtils = require('./utilities/botUtilities.js');
@@ -9,6 +8,11 @@ const configPath = path.join(__dirname, 'config.json');
 const configFile = fs.readFileSync(configPath);
 const config = JSON.parse(configFile);
 const token = config.token;
+const owner = config.owner;
+
+const client = new Commando.Client({
+  owner: owner,
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
