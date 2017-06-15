93efc91ccd5394b9f6c82d35bd866b2437792b16
Brass Tax <brasstax@users.noreply.github.com>
2017-06-14 22:06:23 -0700

    Added lookup for record materia.

---
+++ commands/ffrk/recordMateriaLookup.js
@@ -0,0 +1,39 @@
+const {Command} = require('discord.js-commando');
+const path = require('path');
+botPath = path.join(__dirname,
+  '..', '..', 'utilities', 'record-materia-bot-utils.js');
+const botUtils = require(botPath);
+
+module.exports = class ReplyCommand extends Command {
+  /** constructors a basic ability lookup.
+   * @param {Object} client: discord.js-commando client.
+   **/
+  constructor(client) {
+    super(client, {
+      name: 'record-materia',
+      group: 'ffrk',
+      memberName: 'record-materia',
+      description: 'Looks up a record materia by name.',
+      examples: ['rm Attunement I', 'record-materia Truthseeker',
+        'record Bolt from Above'],
+      args: [
+        {
+          key: 'record_materia',
+          prompt: 'Enter the name of the record materia you want to look up.',
+          type: 'string',
+        },
+      ],
+      aliases: ['record', 'rm'],
+    });
+  }
+
+  /** trigger to run upon invocation.
+   * @param {Object} msg: discord.js-commando message.
+   * @param {Array} args: args from the user input.
+   * @return {Method} msg.say: string
+   **/
+  run(msg, args) {
+    const {record_materia} = args;
+    return botUtils.recordMateria(msg, record_materia);
+  };
+};
---
+++ utilities/record-materia-bot-utils.js
@@ -0,0 +1,78 @@
+const util = require('util');
+const {RichEmbed} = require('discord.js');
+const jsonQuery = require('json-query');
+const escapeStringRegexp = require('escape-string-regexp');
+const titlecase = require('titlecase');
+
+const fs = require('fs');
+const path = require('path');
+
+const enlirJsonPath = path.join(__dirname, '..', 'enlir_json');
+const enlirRecordMateriaPath = path.join(enlirJsonPath, 'recordMateria.json');
+const enlirRecordMateriaFile = fs.readFileSync(enlirRecordMateriaPath);
+const enlirRecordMateria = JSON.parse(enlirRecordMateriaFile);
+
+/** exports.recordMateria
+ * Retrieves information about a record materia.
+ * @param {object} msg: A message object from the discord.js bot.
+ * @param {string} args: the status to look up.
+ **/
+exports.recordMateria = function lookupRecordMateria(msg, args) {
+  if (args.length < 3) {
+    msg.reply('Search query must be at least three characters.');
+    return;
+  };
+  let query;
+  query = titlecase.toLaxTitleCase(args);
+  query = escapeStringRegexp(query);
+  console.log(`RecordMateria to look up: ${query}`);
+  console.log(`,status caller:` +
+      ` ${msg.author.username}#${msg.author.discriminator}`);
+  let queryString = util.format('[name~/%s/i]', query);
+  console.log(`queryString: ${queryString}`);
+  let result = jsonQuery(queryString, {
+    data: enlirRecordMateria,
+    allowRegexp: true,
+  });
+  if (result.value === null) {
+    msg.channel.send(`RecordMateria ${query} not found.`);
+  } else {
+    sendRichEmbedRecordMateria(result, msg)
+      .catch( (err) => {
+        console.log(`Error with sendRichEmbedRecordMateria: ${err}`);
+      });
+  };
+};
+
+/** sendRichEmbedRecordMateria:
+ * Processes and outputs information about a status effect in
+ * RichEmbed format.
+ * @param {object} result: the result from lookupRecordMateria.
+ * @param {object} msg: discord.js message object.
+ * @return {object} Promise
+ **/
+function sendRichEmbedRecordMateria(result, msg) {
+  return new Promise( (fulfill, reject) => {
+    recordMateria = result.value;
+    let description = (recordMateria.effect !== undefined) ?
+      (recordMateria.effect) : ('N/A');
+    let name = recordMateria.name;
+    let character = recordMateria.character;
+    let unlock = recordMateria.unlockCriteria;
+    let realm = recordMateria.realm;
+    let embed = new RichEmbed()
+      .setTitle(name)
+      .setDescription(description)
+      .setColor('#bfdaff')
+      .addField('Realm', realm, true)
+      .addField('Character', character, true)
+      .addField('Unlock Criteria', unlock);
+    msg.channel.send({embed})
+      .then( (resolve) => {
+        fulfill(resolve);
+      }).catch( (error) => {
+        console.log(`Error in sendRichEmbedRecordMateria: ${error}`);
+        reject(error);
+      });
+  });
+};
