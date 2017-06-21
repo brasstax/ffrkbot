e63be6ccd5e3e950e84823eb520ffdf0ef8cdb69
Brass Tax <brasstax@users.noreply.github.com>
2017-06-21 10:49:55 -0700

    added test for main ability call.

--- test/ability-bot-utils.test.js
+++ test/ability-bot-utils.test.js
@@ -15,27 +15,43 @@ const enlirAbilitiesPath = path.join(enlirJsonPath, 'abilities.json');
 const enlirAbilitiesFile = fs.readFileSync(enlirAbilitiesPath);
 const enlirAbilities = JSON.parse(enlirAbilitiesFile);
 
-
-describe('sendRichEmbedAbility', () => {
-  it('Should construct and send a RichEmbedAbility', () => {
-    let msg = sinon.createStubInstance(discord.Client);
-    msg.channel = sinon.createStubInstance(discord.TextChannel);
-    msg.channel.send = sinon.stub().resolves(null);
-    console.log(msg.channel.send());
-    let query = 'waterga';
-    let queryString = util.format('[name~/%s/i]', query);
-    let result = jsonQuery(queryString, {
-      data: enlirAbilities,
-      allowRegexp: true,
+describe('ability', () => {
+  describe('ability call', () => {
+    it('Should call sendRichEmbedAbility', () => {
+      let msg = sinon.createStubInstance(discord.Client);
+      msg.author = sinon.createStubInstance(discord.User);
+      msg.channel = sinon.createStubInstance(discord.TextChannel);
+      msg.channel.send = sinon.stub().resolves(null);
+      msg.author.username = null;
+      msg.author.discriminator = null;
+      let stub = sinon.stub(botUtils, 'sendRichEmbedAbility');
+      stub.resolves(null);
+      let args = 'waterga';
+      res = botUtils.ability(msg, args);
+      assert.equal(res, null, 'ability called sendRichEmbedAbility');
     });
-    return botUtils.sendRichEmbedAbility(result, msg)
-      .then( (res) => {
-        console.log(res);
-        assert.equal(res, null, 'sendRichEmbedAbility succeeded');
-      }).catch( (err) => {
-        console.log(err);
-        assert.fail(err, null, `sendRichEmbedAbility should return null,` +
-          ` but we got ${err}`);
+  });
+  describe('sendRichEmbedAbility', () => {
+    it('Should construct and send a RichEmbedAbility', () => {
+      let msg = sinon.createStubInstance(discord.Client);
+      msg.channel = sinon.createStubInstance(discord.TextChannel);
+      msg.channel.send = sinon.stub().resolves(null);
+      console.log(msg.channel.send());
+      let query = 'waterga';
+      let queryString = util.format('[name~/%s/i]', query);
+      let result = jsonQuery(queryString, {
+        data: enlirAbilities,
+        allowRegexp: true,
       });
+      return botUtils.sendRichEmbedAbility(result, msg)
+        .then( (res) => {
+          console.log(res);
+          assert.equal(res, null, 'sendRichEmbedAbility succeeded');
+        }).catch( (err) => {
+          console.log(err);
+          assert.fail(err, null, `sendRichEmbedAbility should return null,` +
+            ` but we got ${err}`);
+        });
+    });
   });
 });
