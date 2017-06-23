06ec5e5687d34fa92fb3fefde1185b1435687d06
Brass Tax <brasstax@users.noreply.github.com>
2017-06-23 01:55:30 -0700

    Added test coverage for soulbreak utility command.

--- test/soulbreak-bot-utils.test.js
+++ test/soulbreak-bot-utils.test.js
@@ -33,13 +33,13 @@ describe('soulbreak bot utils testing', () => {
         'sendSoulbreakRichEmbedSummary');
       botUtils.__set__('sendSoulbreakRichEmbedSummary',
         soulbreakSummarySpy);
-      botUtils.soulbreak(msg, query, sbType).then( () => {
+      return botUtils.soulbreak(msg, query, sbType).then( () => {
         assert.equal(soulbreakSummarySpy.calledOnce, true);
       });
     });
     it('should send a plaintext summary of a character\'s soul breaks',
         () => {
-          msg.channel.send = sandbox.stub().rejects(null);
+          msg.channel.send = sandbox.stub().rejects('method stubbed out');
           let query = 'tifa';
           let sbType = 'all';
           soulbreakSummarySpy = sandbox.spy(botUtils,
@@ -50,10 +50,52 @@ describe('soulbreak bot utils testing', () => {
             'sendSoulbreakRichEmbedSummary': soulbreakSummarySpy,
             'sendSoulbreakPlaintextSummary': soulbreakPlaintextSpy,
           });
-          botUtils.soulbreak(msg, query, sbType).then( () => {
+          return botUtils.soulbreak(msg, query, sbType).catch( () => {
             assert.equal(soulbreakSummarySpy.calledOnce, true);
             assert.equal(soulbreakPlaintextSpy.calledOnce, true);
           });
       });
+    it('should inform when search query is less than three letters', () => {
+      let message = 'Character name must be at least three characters.';
+      msg.channel.send = sandbox.stub().resolves(null);
+      let query = 'ba';
+      return botUtils.soulbreak(msg, query, 'all').then( () => {
+        assert.equal(msg.channel.send.calledOnce, true);
+        assert.equal(msg.channel.send.calledWith(message), true);
+      });
+    });
+    it('should inform when no results are found for a character', () => {
+      let message = `No results for 'Foobar' 'all'.`;
+      let query = 'foobar';
+      msg.channel.send = sandbox.stub().resolves(null);
+      return botUtils.soulbreak(msg, query, 'all').then( () => {
+        assert.equal(msg.channel.send.calledOnce, true);
+        assert.equal(msg.channel.send.calledWith(message), true);
+      });
+    });
+    it('should inform when a user has not put in a valid SB type', () => {
+      let message = 'Soulbreak type not one of: ' +
+        'All, Default, SB, SSB, BSB, USB, OSB, CSB.';
+      msg.channel.send = sandbox.stub().resolves(null);
+      let query = 'tifa';
+      return botUtils.soulbreak(msg, query, 'aaaaaaa').then( () => {
+        assert.equal(msg.channel.send.calledOnce, true);
+        assert.equal(msg.channel.send.calledWith(message), true);
+      });
+    });
+    it('should send one RichEmbed for a default soulbreak', () => {
+      let query = 'tifa';
+      msg.channel.send = sandbox.stub().resolves(null);
+      return botUtils.soulbreak(msg, query, 'default').then( () => {
+        assert.equal(msg.channel.send.calledOnce, true);
+      });
+    });
+    it('should send multiple for characters with more than one SB', () => {
+      let query = 'garnet';
+      msg.channel.send = sandbox.stub().resolves(null);
+      return botUtils.soulbreak(msg, query, 'BSB').then( () => {
+        assert.isAbove(msg.channel.send.callCount, 1);
+      });
+    });
   });
 });
