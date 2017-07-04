833766dcb890377c97ad81e5965ce28d65ef06dc
Brass Tax <brasstax@users.noreply.github.com>
2017-07-04 13:31:42 -0700

    update-json.sh now takes legend materia into account.

--- update-json.sh
+++ update-json.sh
@@ -4,6 +4,4 @@ gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Soul Breaks" -b enlir_js
 gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Status" -b enlir_json/status.json
 gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Abilities" -b enlir_json/abilities.json
 gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Commands" -b enlir_json/bsbCommands.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Commands" -b enlir_json/bsbCommands.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Abilities" -b enlir_json/abilities.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Soul Breaks" -b enlir_json/soulbreaks.json
+gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Legend Materia" -b enlir_json/legendMateria.json
