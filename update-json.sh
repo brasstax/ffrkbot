5b50a27449ca98cf34863a203135e324f68605b3
Brass Tax <brasstax@users.noreply.github.com>
2018-06-09 11:31:07 -0700

    Enlir renamed the Commands sheet to Burst. update-json.sh now reflects that.

--- update-json.sh
+++ update-json.sh
@@ -3,6 +3,6 @@ gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Record Materia" -b enlir
 gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Soul Breaks" -b enlir_json/soulbreaks.json
 gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Status" -b enlir_json/status.json
 gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Abilities" -b enlir_json/abilities.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Commands" -b enlir_json/bsbCommands.json
+gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Burst" -b enlir_json/bsbCommands.json
 gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Legend Materia" -b enlir_json/legendMateria.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Burst" -b enlir_json/other.json
+gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Other" -b enlir_json/other.json
