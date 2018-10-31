099cddd1a4d93c7eafc62f7d4d2b7aeecf2c698a
Brass Tax <brasstax@users.noreply.github.com>
2018-10-31 09:19:35 -0700

    Updated tests for AASB.

--- test/soulbreak-bot-utils.test.js
+++ test/soulbreak-bot-utils.test.js
@@ -75,7 +75,8 @@ describe('soulbreak bot utils testing', () => {
     });
     it('should inform when a user has not put in a valid SB type', () => {
       let message = 'Soulbreak type not one of: ' +
-        'All, Default, SB, SSB, BSB, USB, OSB, CSB, FSB, UOSB, Glint, ASB.';
+        'All, Default, SB, SSB, BSB, USB, OSB, CSB, FSB, ' +
+        'UOSB, Glint, AOSB, AASB.';
       msg.channel.send = sandbox.stub().resolves(null);
       let query = 'tifa';
       return botUtils.soulbreak(msg, query, 'aaaaaaa').then( () => {
