599b7a8db7107ddd3b450b4df3847a6dffe8801c
drgoonic <36549130+drgoonic@users.noreply.github.com>
2018-11-18 23:32:12 -0500

    SpeedRank added
    Added the "rank" command to allow speedrunners to pull their specific stats in the general categories.
    
    The new command has the form:
    rank <playername> <category> <type>
    playername = name in the speedrun sheet
    category = 3star, 4star, 5star, or Torment (defaults to 4star)
    type = overall or no-csb (defaults to overall)

---
+++ commands/misc/speedrank.js
@@ -0,0 +1,62 @@
+const {Command} = require('discord.js-commando');
+const path = require('path');
+speedrankPath = path.join(__dirname,
+  '..', '..', 'utilities', 'speedrank-utils.js');
+const speedrankUtils = require('../../utilities/speedrank-utils.js');
+
+module.exports = class ReplyCommand extends Command {
+  /** constructor for looking up soulbreaks.
+   * @param {Object} client: discord.js-commando client.
+   **/
+  constructor(client) {
+    super(client, {
+      name: 'rank',
+      group: 'misc',
+      memberName: 'speedrank',
+      description: 'Looks up all the entries for a given speedrunner in a specified leaderboard' +
+      ' from the FFRecordKeeper Discord Speedrun challenge leaderboard' +
+      ' (https://docs.google.com/spreadsheets/d/11gTjAkpm4D3uoxnYCN7ZfbiVnKyi7tmm9Vp9HvTkGpw).',
+      examples: ['rank EverythingIsGravy 5star', 'top 10 no-csb', 'top 5 cod', 'top 3 Maliris'],
+      args: [
+        {
+          key: 'name',
+          prompt: 'Enter the speedrunner you want to query.' +
+          ' Make sure you use the name as it appears in the spreadsheet.',
+          type: 'string',
+          default: 'EverythingIsGravy',
+        },
+        {
+          key: 'category',
+          prompt: 'Enter the name of the category you want to look up.' +
+          ' (Can be "5star", "4star", "3star", or "Torment" ' +
+          ' on the chart. The default is "4star")',
+          type: 'string',
+          default: '4star',
+        },
+        {
+          key: 'secondaryCategory',
+          prompt: 'Specify if you want the overall or no-csb ranking' +
+          ' no-csb does not apply to Torment' +
+          ' (defaults to "overall".)',
+          type: 'string',
+          default: 'overall',
+        },
+      ],
+      aliases: ['speedrank'],
+    });
+  }
+
+  /** trigger to run upon invocation.
+   * @param {Object} msg: discord.js-commando message.
+   * @param {Array} args: args from the user input.
+   * @return {Method} msg.say: string
+   **/
+  run(msg, args) {
+    console.log('Im in ur msg');
+    const {name, category, secondaryCategory} = args;
+    return speedrankUtils.speedrank(msg, name, category, secondaryCategory)
+      .catch( (err) => {
+        console.log('error in speedrank command:', err);
+      });
+  };
+};
