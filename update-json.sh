f36456299fbcb8e4a11fd9912b38f2826831631d
Brass Tax <brasstax@users.noreply.github.com>
2018-12-31 21:39:26 -0800

    Point to new community spreadsheet for updates.

--- update-json.sh
+++ update-json.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Record Materia" -b enlir_json/recordMateria.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Soul Breaks" -b enlir_json/soulbreaks.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Status" -b enlir_json/status.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Abilities" -b enlir_json/abilities.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Brave" -b enlir_json/braveCommands.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Burst" -b enlir_json/bsbCommands.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Legend Materia" -b enlir_json/legendMateria.json
-gsjson 16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ -w "Other" -b enlir_json/other.json
+gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Record Materia" -b enlir_json/recordMateria.json
+gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Soul Breaks" -b enlir_json/soulbreaks.json
+gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Status" -b enlir_json/status.json
+gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Abilities" -b enlir_json/abilities.json
+gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Brave" -b enlir_json/braveCommands.json
+gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Burst" -b enlir_json/bsbCommands.json
+gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Legend Materia" -b enlir_json/legendMateria.json
+gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Other" -b enlir_json/other.json
