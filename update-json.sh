182cc01eae48755c44e0ff876a11a1555ef3ba35
elninofr <elninofr@users.noreply.github.com>
2019-03-04 22:05:44 +0100

    Update update-json.sh
    define bot folder for autostart call
    echo progress

--- update-json.sh
+++ update-json.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Record Materia" -b enlir_json/recordMateria.json
+echo "RM done"
 gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Soul Breaks" -b enlir_json/soulbreaks.json
+echo "SB done"
 gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Status" -b enlir_json/status.json
+echo "status done"
 gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Abilities" -b enlir_json/abilities.json
+echo "ability done"
 gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Brave" -b enlir_json/braveCommands.json
+echo "brave done"
 gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Burst" -b enlir_json/bsbCommands.json
+echo "bsb cmd done"
 gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Legend Materia" -b enlir_json/legendMateria.json
+echo "LM done"
 gsjson 1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk -w "Other" -b enlir_json/other.json
+echo "All done"
